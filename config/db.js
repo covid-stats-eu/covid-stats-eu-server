8cd1222d039c6e8a6677b6af9ce142531844b293
Rigas-A <p18riga@ionio.gr>
2021-03-21 18:33:26 +0200

    Add db connection

--- config/db.js
+++ config/db.js
@@ -1,1 +1,14 @@
 // Database connection and configuration
+const mysql = require('mysql2/promise');
+const credentials = require('./credentials.js');
+
+async function query(sql, params) {
+    const connection = await mysql.createConnection(credentials.db);
+    const [results, ] = await connection.execute(sql, params);
+
+    return results;
+}
+
+module.exports = {
+    query
+}
