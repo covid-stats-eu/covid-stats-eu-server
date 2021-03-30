52f0918266671760e0a46c535095e71c3e758672
Rigas-A <p18riga@ionio.gr>
2021-03-30 15:56:46 +0300

    Added activity getByCountry QA

--- models/activity.js
+++ models/activity.js
@@ -4,4 +4,11 @@ const getAll = async () => {
     return await query('SELECT cases, deaths, tests FROM activity')
 }
 
-export default getAll
+const getByCountry = async (countryCode) => {
+    return await query('SELECT sum(cases), sum(deaths), sum(tests) FROM activity WHERE code="' + countryCode + '";')
+}
+
+export {
+    getAll,
+    getByCountry,
+};
--- routes/activity.js
+++ routes/activity.js
@@ -1,10 +1,14 @@
 import Router from 'express'
-import getAll from '../models/activity.js'
+import * as activity from '../models/activity.js'
 
 const router = Router()
 
 router.get('/getAll', async (req, res) => {
-    res.json(await getAll())
+    res.json(await activity.getAll())
+})
+
+router.get('/getByCountry/:countryCode', async (req, res) => {
+    res.json(await activity.getByCountry(req.params.countryCode))
 })
 
 export default router
